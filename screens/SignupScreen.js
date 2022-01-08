7940c5b7bad5ebba3a759bffec4b4db024ece573
Samana Shrestha <samanashrestha02@gmail.com>
2022-01-08 17:42:02 +0545

    signup

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,22 +1,19 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
-import navigation from "../navigation";
-import { auth } from "../firebase";
 import firebase from "firebase";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Icon } from "react-native-elements";
+import { auth } from "../firebase";
+
 import {
   StyleSheet,
   Text,
   View,
   Image,
   TextInput,
-  Button,
   TouchableOpacity,
 } from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
-import { add } from "react-native-reanimated";
+import { Icon } from "react-native-elements/dist/icons/Icon";
 
 export default function SignupScreen() {
   const [name, setName] = useState("");
@@ -32,7 +29,15 @@ export default function SignupScreen() {
     check_textInputChange: false,
     secureTextEntry: true,
   })
-  
+
+
+  const updateSecureTextEntry = () => {
+    setData({
+      ...data,
+      secureTextEntry: !data.secureTextEntry,
+    });
+  }
+
   const handleSignup = () => {
     if (password!==cpassword){
       alert("Password dont match")
@@ -50,3 +55,20 @@ export default function SignupScreen() {
         phonenumber:phonenumber
       }
 
+
+      const userRef= firebase.firestore().collection('users')
+      userRef
+        .doc(uid)
+        .set(data)
+        .then(() => {
+          navigation.navigate('Home, {user: data')
+          
+        })
+        .catch((error) => {
+          alert(error)
+        })
+    })
+    .catch((error) => {
+      alert(error)
+    })
+  }
