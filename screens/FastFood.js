d124d45d0cdec06c90d55c7cb45f335ac6433856
ACER <210220@softwarica.edu.np>
2022-01-11 00:07:46 +0600

    import

---
+++ screens/FastFood.js
@@ -0,0 +1,9 @@
+import { View, Text, Image, StyleSheet } from 'react-native';
+import React from 'react';
+import { ScrollView } from 'react-native-gesture-handler';
+import { Divider } from 'react-native-elements/dist/divider/Divider';
+import BouncyCheckbox from 'react-native-bouncy-checkbox';
+import ViewCart from '../components/restaurantDetail/ViewCart'
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+
