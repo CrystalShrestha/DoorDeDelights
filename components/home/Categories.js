0e5af4980604ab9e2547cae529139662c18c8246
bipinaghimire <bipinaghimire6@gmail.com>
2022-01-22 13:54:08 +0545

    categories

--- components/home/Categories.js
+++ components/home/Categories.js
@@ -7,7 +7,14 @@ const items = [
     image1: require("../../assets/images/shopping-bag.png"),
     text1: "Pick-up",
   },
-
+  {
+   image2: require("../../assets/images/soft-drink.png"),
+    text2: "Soft Drinks",
+  },
+  {
+    image3: require("../../assets/images/bread.png"),
+    text3: "Bakery Items",
+  },
 ];
 
 export default function Categories() {
@@ -36,7 +43,41 @@ export default function Categories() {
             <Text style={{ fontSize: 13, fontWeight: "900" }}>Pickup</Text>
           </View>
         </TouchableOpacity>
-      
+       {/* Softdrinks */}
+       <TouchableOpacity >
+          <View style={{ alignItems: "center", marginRight: -55 }}>
+            <Image
+              source={require("../../assets/images/soft-drink.png")}
+              style={{
+                width: 50,
+                height: 40,
+                resizeMode: "contain",
+                marginRight: 100,
+              }}
+            />
+            <Text style={{ fontSize: 13, fontWeight: "900", marginLeft: -85 }}>
+              Softdrinks
+            </Text>
+          </View>
+        </TouchableOpacity>
+
+        {/* Bakery */}
+        <TouchableOpacity >
+          <View style={{ alignItems: "center", marginRight: -55 }}>
+            <Image
+              source={require("../../assets/images/bread.png")}
+              style={{
+                width: 50,
+                height: 40,
+                resizeMode: "contain",
+                marginRight: 100,
+              }}
+            />
+            <Text style={{ fontSize: 13, fontWeight: "900", marginLeft: -85 }}>
+              Bakery{" "}
+            </Text>
+          </View>
+        </TouchableOpacity>
       </ScrollView>
     </View>
   );
