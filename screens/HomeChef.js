cc10a325267edd2d8518a8762b72a9e5df0f86a6
bipinaghimire <bipinaghimire6@gmail.com>
2022-01-26 16:58:22 +0545

    Home Chef

--- screens/HomeChef.js
+++ screens/HomeChef.js
@@ -6,51 +6,51 @@ import { Divider } from 'react-native-elements/dist/divider/Divider';
 
 const chefs=[
   {
-  title: "",
+  title: "Chef Noah",
   description:'',
-  contact:'',
+  contact:'98563214',
   image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWpcL8mapuA3ILF0XeuOj4vSIq4RQDd6AFlA&usqp=CAU'
 },
 {
-  title: "",
+  title: "Chef Anna",
   description:'',
-  contact:'',
+  contact:'987456321',
   image: 'https://st4.depositphotos.com/22230270/23669/v/1600/depositphotos_236693120-stock-illustration-illustration-female-chef-cartoon.jpg'
 },
 {
-  title: "",
+  title: "Chef John",
   description:'',
-  contact:'',
+  contact:'9865325632',
   image: 'https://image.freepik.com/free-vector/cute-chef-boy-holding-ladle-soup-chef-cartoon_296684-321.jpg'
 },
 {
-  title: "",
+  title: "Chef Oliver",
   description:'',
-  contact:'',
+  contact:'984563216',
   image: 'https://img.freepik.com/free-vector/cute-chef-boy-holding-pizza-chef-cartoon_296684-319.jpg?size=338&ext=jpg'
 },
 {
-  title: "",
+  title: "Chef Edward",
   description:'',
-  contact:'',
+  contact:'986125369',
   image: 'https://static.vecteezy.com/system/resources/previews/002/042/980/original/cartoon-pizza-chef-vector.jpg'
 },
 {
-  title: "",
+  title: "Chef Sophie",
   description:'',
-  contact:'',
+  contact:'987456325',
   image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCOCylq3PGQ1fsDxQKPOgzQY2qe1FEkQfLxg&usqp=CAU'
 },
 {
-  title: "",
+  title: "Chef Bob",
   description:'',
-  contact:'',
+  contact:'985632147',
   image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0pYT4iiHZITcnj8vi0ry_mzdm3eVtEQebfA&usqp=CAU'
 },
 {
-  title: "",
+  title: "Chef Edward",
   description:'',
-  contact:'',
+  contact:'987456321',
   image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqZHCpUJnnaptDUCgtPeqDRI0fmm7vnaigng&usqp=CAU'
 },
 
