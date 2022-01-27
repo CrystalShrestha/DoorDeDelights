7b61eee96a18edbc0f4ba171dd2a11b2dd9948dd
astikagrg <grgastika@gmail.com>
2022-01-27 19:25:17 +0545

    OH

--- screens/OrderHistory.js
+++ screens/OrderHistory.js
@@ -3,15 +3,3 @@ import { View, Text, SafeAreaView, ScrollView } from "react-native";
 import { Divider } from "react-native-elements";
 import BottomTabs from "../components/home/BottomTabs";
 
-export default function OrderHistory() {
-  return (
-    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
-      <View style={{ backgroundColor: "white", padding: 15 }}></View>
-      <ScrollView showsVerticalScrollIndicator={false}>
-        {/* Yaha Hardcode GARNE! */}
-      </ScrollView>
-      <Divider width={1} />
-      <BottomTabs />
-    </SafeAreaView>
-  );
-}
