22fac6a4fa2741e923c760defc1856314716be3d
Samana Shrestha <samanashrestha02@gmail.com>
2022-01-30 15:30:05 +0545

    navigation

--- navigation.js
+++ navigation.js
@@ -24,5 +24,43 @@ import VerifyEmail from "./screens/VerifyEmail";
 import Aboutus from "./screens/Aboutus";
 
 const store = configureStore();
+export default function RootNavigation() {
+  const Stack = createStackNavigator();
 
+  const screenOptions = {
+    headerShown: false,
+  };
+
+  return (
+    <ReduxProvider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="LoginScreen" screenOptions={screenOptions}>
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Login"
+            component={LoginScreen}
+          />
+          <Stack.Screen name="SignupScreen" component={SignupScreen} />
+          <Stack.Screen name="ForgetPassword" component={Forgetpassword} />
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="SearchHistory" component={SearchHistory} />
+          <Stack.Screen name="HomeChef" component={HomeChef} />
+          <Stack.Screen name="OrderHistory" component={OrderHistory} />
+          <Stack.Screen name="UserAccount" component={UserAccount} />
+          <Stack.Screen name="RestaurantDetail" component={RestaurantDetail} />
+          <Stack.Screen name="OrderCompleted" component={OrderCompleted} />
+          <Stack.Screen name="Pickup" component={Pickup} />
+          <Stack.Screen name="Softdrinks" component={Softdrinks} />
+          <Stack.Screen name="Bakery" component={Bakery} />
+          <Stack.Screen name="CoffeeTea" component={CoffeeTea} />
+          <Stack.Screen name="Deals" component={Deals} />
+          <Stack.Screen name="Fastfood" component={Fastfood} />
+          <Stack.Screen name="Desserts" component={Desserts} />
+          <Stack.Screen name="VerifyEmail" component={VerifyEmail} />
+          <Stack.Screen name="Aboutus" component={Aboutus} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ReduxProvider>
+  );
+}
 
