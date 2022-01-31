cdc918e5fac5329b1396c512f04046698abfbe33
Crystal Shrestha <crystalstha91@gmail.com>
2022-02-01 00:23:13 +0545

    Hardcode left

---
+++ screens/Softdrinks.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const Softdrinks = () => {
+  return (
+    <View>
+      <Text>This is for soft drinks</Text>
+    </View>
+  );
+};
+
+export default Softdrinks;
+
+const styles = StyleSheet.create({});
