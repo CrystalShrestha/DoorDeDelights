0d1a1b3bd859c8d4d9ddcc0133ddf95a333f3759
bipinaghimire <bipinaghimire6@gmail.com>
2022-02-02 18:42:29 +0545

    firebase

--- firebase.js
+++ firebase.js
@@ -11,15 +11,5 @@ const firebaseConfig = {
 
 !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
 
-let app;
-if (firebase.apps.length === 0) {
-  app = firebase.initializeApp(firebaseConfig);
-} else {
-  app = firebase.app()
-}
 
-const auth = firebase.auth()
 
-export { auth };
-
-export default firebase;
